295647cd7c6bf5d16dd5c1697fbfae207207b4f0
lciotti <lciotti@alionscience.com>
2022-04-25 09:09:38 -0400

    Updatesto configuration workds and included A8 module.

--- SBR_HELPER.ino
+++ SBR_HELPER.ino
@@ -5,7 +5,7 @@ String sdata="";
 void setup() {
   // put your setup code here, to run once:
 
-  Serial.begin(112149, SERIAL_8E1); 
+  Serial.begin(9600); 
   Serial1.begin(112149, SERIAL_8E1);
   Serial.setTimeout(250);
   digitalWrite(2,HIGH);
@@ -15,7 +15,7 @@ void setup() {
   digitalWrite(4,HIGH);
   delay(100);
   digitalWrite(5,HIGH);
-  delay(100);
+  delay(100);\
   digitalWrite(6,HIGH);
   delay(100);
   digitalWrite(7,HIGH);
@@ -65,11 +65,11 @@ void loop() {
         comcheck = true;
         Serial1.write(0x9D);
         Serial1.write(0x01);
-        Serial1.write(0x53);
-        Serial1.write(0x64);
-        Serial1.write(0x64);
+        Serial1.write(0x5E);
+        Serial1.write(0x11);
+        Serial1.write(0x2F);
         Serial1.write(0xC8);
-        Serial1.write(0x8C);
+        Serial1.write(0x04);
       }
       else if (sdata.indexOf("A1Enable")>=0)
       {
@@ -97,11 +97,12 @@ void loop() {
         Serial1.write(0xAD);
         Serial1.write(0x01);
         Serial1.write(0x56);
-        Serial1.write(0x0A);
-        Serial1.write(0x0A);
-        Serial1.write(0x41);
-        Serial1.write(0x59);
+        Serial1.write(0x09);
+        Serial1.write(0x1D);
+        Serial1.write(0x32);
+        Serial1.write(0x5C);
       }
+
       else if (sdata.indexOf("A2Enable")>=0)
       {
         Serial1.write(0xA9);
@@ -128,9 +129,9 @@ void loop() {
         Serial1.write(0xB5);
         Serial1.write(0x01);
         Serial1.write(0x65);
-        Serial1.write(0x0A);
-        Serial1.write(0x0A);
-        Serial1.write(0x41);
+        Serial1.write(0x05);
+        Serial1.write(0x1E);
+        Serial1.write(0x32);
         Serial1.write(0x70);
       }
       else if (sdata.indexOf("A3Enable")>=0)
@@ -159,9 +160,9 @@ void loop() {
         Serial1.write(0x3D);
         Serial1.write(0x01);
         Serial1.write(0x65);
-        Serial1.write(0x0A);
-        Serial1.write(0x0A);
-        Serial1.write(0x41);
+        Serial1.write(0x05);
+        Serial1.write(0x1E);
+        Serial1.write(0x32);
         Serial1.write(0xF8);
       }
       else if (sdata.indexOf("A4Enable")>=0)
@@ -190,10 +191,10 @@ void loop() {
         Serial1.write(0x75);
         Serial1.write(0x01);
         Serial1.write(0x83);
-        Serial1.write(0x0A);
-        Serial1.write(0x0A);
-        Serial1.write(0x41);
-        Serial1.write(0x4E);
+        Serial1.write(0x03);
+        Serial1.write(0x21);
+        Serial1.write(0x32);
+        Serial1.write(0x4F);
       }
       else if (sdata.indexOf("A5Enable")>=0)
       {
@@ -221,10 +222,10 @@ void loop() {
         Serial1.write(0xCD);
         Serial1.write(0x01);
         Serial1.write(0x83);
-        Serial1.write(0x0A);
-        Serial1.write(0x0A);
-        Serial1.write(0x41);
-        Serial1.write(0xA6);
+        Serial1.write(0x03);
+        Serial1.write(0x21);
+        Serial1.write(0x32);
+        Serial1.write(0xA7);
       }
       else if (sdata.indexOf("A6Enable")>=0)
       {
@@ -252,10 +253,10 @@ void loop() {
         Serial1.write(0xD5);
         Serial1.write(0x01);
         Serial1.write(0x83);
-        Serial1.write(0x0A);
-        Serial1.write(0x0A);
-        Serial1.write(0x41);
-        Serial1.write(0xAE);
+        Serial1.write(0x03);
+        Serial1.write(0x21);
+        Serial1.write(0x32);
+        Serial1.write(0xAF);
       }
       else if (sdata.indexOf("A7Enable")>=0)
       {
@@ -277,12 +278,99 @@ void loop() {
         Serial1.write(0x02);
         Serial1.write(0xD3);
       }
+    else if (sdata.indexOf("A8Init")>=0)
+      {
+        comcheck = true;
+        Serial1.write(0xD5);
+        Serial1.write(0x01);
+        Serial1.write(0x56);
+        Serial1.write(0x09);
+        Serial1.write(0x1D);
+        Serial1.write(0x3C);
+        Serial1.write(0x16);
+      }
+      else if (sdata.indexOf("A8Enable")>=0)
+      {
+        Serial1.write(0x59);
+        Serial1.write(0x44);
+        Serial1.write(0x0D);
+        digitalWrite(8,HIGH);
+      }
+      else if (sdata.indexOf("A8Disable")>=0)
+      {
+        Serial1.write(0x59);
+        Serial1.write(0x84);
+        Serial1.write(0xDD);
+        digitalWrite(8,LOW);
+      }
+      else if (sdata.indexOf("A8Status")>=0)
+      {
+        Serial1.write(0x59);
+        Serial1.write(0x02);
+        Serial1.write(0x5B);
+      }
+      
+
+
+      else if (sdata.indexOf("DisableAll")>=0)
+      {
+        comcheck = true;
+        // Disable A1
+        Serial1.write(0x99);
+        Serial1.write(0x84);
+        Serial1.write(0x1D);
+        digitalWrite(2,LOW);
+        delay(1);
+        // Disable A2
+        Serial1.write(0xA9);
+        Serial1.write(0x84);
+        Serial1.write(0x2D);
+        digitalWrite(3,LOW);
+        delay(1);
+        // Disable A3
+        Serial1.write(0xB1);
+        Serial1.write(0x84);
+        Serial1.write(0x35);
+        digitalWrite(4,LOW);
+        delay(1);
+        // Disable A4
+        Serial1.write(0x39);
+        Serial1.write(0x44);
+        Serial1.write(0x7D);
+        digitalWrite(5,HIGH);
+        delay(1);
+        // Disable A5
+        Serial1.write(0x71);
+        Serial1.write(0x84);
+        Serial1.write(0xF5);
+        digitalWrite(6,LOW);
+        delay(1);
+        // Disable A6
+        Serial1.write(0xC9);
+        Serial1.write(0x84);
+        Serial1.write(0x4D);
+        digitalWrite(7,LOW);
+        delay(1);
+        // Disable A7
+        Serial1.write(0xD1);
+        Serial1.write(0x84);
+        Serial1.write(0x55);
+        digitalWrite(8,LOW);
+        delay(1);
+        // Disable A8
+        Serial1.write(0x59);
+        Serial1.write(0x84);
+        Serial1.write(0xDD);
+        digitalWrite(9,LOW);
+
+      }
+      
       else
       {
           Serial1.print(sdata); 
       }
     
-
+      sdata="";
   
   }
 
